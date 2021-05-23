287aac7a92c21ef7813014cb1c725d8805f12b75
Konstantin Shulga <mrkonstantinsh@gmail.com>
2021-05-23 13:33:04 +0300

    feat(xamarin): modify menu & add a blank settings page template

--- XamarinApp/XamarinApp/XamarinApp/AppShell.xaml.cs
+++ XamarinApp/XamarinApp/XamarinApp/AppShell.xaml.cs
@@ -13,6 +13,7 @@ namespace XamarinApp
             InitializeComponent();
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            Routing.RegisterRoute(nameof(SettingsPage), typeof(SettingsPage));
         }
 
     }
